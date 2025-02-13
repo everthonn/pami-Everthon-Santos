5bb2b7fdbfcff89b1bf226fc6e28f04a48ad8f7e
Everthon <everthondier4@gmail.com>
2025-02-13 08:32:31 -0300

    Cria o arquivo console.js da aula 01

---
+++ aula 01/console.js
@@ -0,0 +1,12 @@
+console.log("Ola Mundo")
+
+var A = "oi" //não utilizada
+let B = "tudo bem?" //vai existir apenas aonde foi criada
+const C = 3 //atribui coisas apenas uma vez, não pode ser alterada e é vista no projeto inteiro 
+const D = 4
+// diferença de escopo
+
+console.log(A, B)
+console.log(A + B)
+
+console.log(C + D)
